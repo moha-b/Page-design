f6cfb7182ab07e4a4232b2b4bb055ed504f69b19
M O H A B <73842931+moha-b@users.noreply.github.com>
2022-04-24 04:52:51 +0200

    update

--- lib/profile.dart
+++ lib/profile.dart
@@ -19,7 +19,7 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
-Color blue = Color(0xFFE7C6FF);
+Color blue = Color(0xFFe493d5);
 
 class _ProfilePageState extends State<ProfilePage> {
   @override
@@ -27,63 +27,114 @@ class _ProfilePageState extends State<ProfilePage> {
     //final width = MediaQuery.of(context).size.width;
     //final height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Column(
+      body: Wrap(
         children: [
-          Container(
-            child: Stack(
-              children: [
-                Opacity(
-                  opacity: 0.5,
-                  child: ClipPath(
-                    clipper: Wave(),
-                    child: Container(
-                      color: blue,
-                      height: 240,
-                      alignment: Alignment.center,
+          Column(
+            children: [
+              //the curves
+              Container(
+                child: Stack(
+                  children: [
+                    Opacity(
+                      opacity: 0.5,
+                      child: ClipPath(
+                        clipper: Wave(),
+                        child: Container(
+                          color: blue,
+                          height: 240,
+                          alignment: Alignment.center,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                ClipPath(
-                  clipper: Wave(),
-                  child: Container(
-                    color: blue,
-                    height: 210,
-                    alignment: Alignment.center,
-                  ),
-                ),
-                Container(
-                  child: CircleAvatar(
-                    radius: 61,
-                    backgroundImage: AssetImage('images/TokyoGhoul.jpg'),
-                  ),
-                  alignment: Alignment.center,
-                  height: 340,
-                ),
-                Container(
-                    height: 180,
-                    margin: EdgeInsets.only(right: 180),
-                    child: IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.arrow_back_rounded,
-                          size: 38,
-                          color: Colors.white,
-                        ))),
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 20),
-            child: Card(
-              child: ListTile(
-                leading: Icon(Icons.settings_accessibility_outlined),
-                title: Text(
-                  "Edit Profile",
-                  style: TextStyle(),
+                    ClipPath(
+                      clipper: Wave(),
+                      child: Container(
+                        color: blue,
+                        height: 210,
+                        alignment: Alignment.center,
+                      ),
+                    ),
+                    Container(
+                      child: CircleAvatar(
+                        radius: 61,
+                        backgroundImage: AssetImage('images/TokyoGhoul.jpg'),
+                      ),
+                      alignment: Alignment.center,
+                      height: 340,
+                    ),
+                    Container(
+                        height: 180,
+                        margin: EdgeInsets.only(right: 180),
+                        child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.arrow_back_rounded,
+                              size: 38,
+                              color: Colors.white,
+                            ))),
+                    Container(
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(top: 240),
+                      child: Column(children: [
+                        Text("Mohab",style: TextStyle(fontSize: 30,color: blue),),
+                        Text("MohabKhalid21@gmail.com",style: TextStyle(fontSize: 15,color: blue),)
+                      ],),
+                    )
+                  ],
                 ),
               ),
-            ),
-          )
+              // Setting
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: Card(
+                  color: blue,child: InkWell(
+                    onTap: () {},
+                    child: ListTile(
+                      leading: Icon(Icons.settings_outlined,color: Colors.white,),
+                      title: Text(
+                        "Settings",
+                        style: TextStyle(color:Colors.white),
+                      ),
+                    ),
+                  ),shape: OutlineInputBorder(borderRadius: BorderRadius.circular(20),borderSide: BorderSide(color: blue)),
+                ),
+              ),
+              Divider(height: 20,),
+              // personal Info
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: Card(
+                  color: blue,child: InkWell(
+                  onTap: () {},
+                  child: ListTile(
+                    leading: Icon(Icons.person_pin,color: Colors.white,),
+                    title: Text(
+                      "Edit Profile",
+                      style: TextStyle(color:Colors.white),
+                    ),
+                  ),
+                ),shape: OutlineInputBorder(borderRadius: BorderRadius.circular(20),borderSide: BorderSide(color: blue)),
+                ),
+              ),
+              Divider(height: 20,),
+              // notifications
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: Card(
+                  color: blue,child: InkWell(
+                  onTap: () {},
+                  child: ListTile(
+                    leading: Icon(Icons.notifications,color: Colors.white,),
+                    title: Text(
+                      "Notifications",
+                      style: TextStyle(color:Colors.white),
+                    ),
+                  ),
+                ),shape: OutlineInputBorder(borderRadius: BorderRadius.circular(20),borderSide: BorderSide(color: blue)),
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );
@@ -113,6 +164,6 @@ class Wave extends CustomClipper<Path> {
 
   @override
   bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    throw UnimplementedError();
+    return true;
   }
 }
